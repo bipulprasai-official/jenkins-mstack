df8b63ade2615ee2172be49cfaba62b562a19c46
bipul prasai <bipulprasai2020@gmail.com>
2023-01-15 23:26:01 +1100

    added port number on server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-
+const port  = 4500
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log('Example app listening on port port!');
+    console.log(`Example app listening on port ${port}!`);
 });
 
 //Run app, then load http://localhost:port in a browser to see the output.
